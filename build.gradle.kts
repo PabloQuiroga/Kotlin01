2c05a7b54d9a660e2d777d7147e84f8a8b6fce9a
Pablo Quiroga <jtdoe78@gmail.com>
2026-06-14 10:36:37 -0300

    Add Jacoco Reports

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    kotlin("jvm") version "2.0.0"
+    kotlin("jvm") version "2.2.21"
+    alias(libs.plugins.jacoco)
 }
 
 group = "org.example"
@@ -21,4 +22,20 @@ tasks.test {
 
 kotlin {
     jvmToolchain(21)
+}
+
+// Configuración de JaCoCo
+jacoco {
+    toolVersion = "0.8.12" // La versión de la herramienta JaCoCo sí se especifica aquí
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // Asegura que los tests se ejecuten antes del informe
+    reports {
+        xml.required.set(true)
+        csv.required.set(false)
+        html.required.set(true)
+    }
+    // Esto es importante para que SonarQube encuentre el informe
+    executionData.setFrom(fileTree(project.buildDir).include("jacoco/*.exec"))
 }
