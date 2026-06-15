155e8b0e54b908fb6fd8a719116c3c9c76b899ee
Pablo Quiroga <jtdoe78@gmail.com>
2026-06-15 10:58:36 -0300

    Add Detekt with rules

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "2.2.21"
     alias(libs.plugins.jacoco)
+    //alias(libs.plugins.detekt) // Aplicamos el plugin de Detekt usando alias
 }
 
 group = "org.example"
@@ -10,10 +11,13 @@ repositories {
     mavenCentral()
 }
 
+val detekt by configurations.creating
+
 dependencies {
     testImplementation(kotlin("test"))
     implementation(libs.sqlite.jdbc)
     testImplementation(libs.mockk) // Añadimos la dependencia de MockK para tests
+    detekt("dev.detekt:detekt-cli:2.0.0-alpha.4")
 }
 
 tasks.test {
@@ -37,5 +41,29 @@ tasks.jacocoTestReport {
         html.required.set(true)
     }
     // Esto es importante para que SonarQube encuentre el informe
-    executionData.setFrom(fileTree(project.buildDir).include("jacoco/*.exec"))
+    executionData.setFrom(fileTree(layout.buildDirectory.get().asFile).include("jacoco/*.exec"))
+}
+
+//detekt {
+//    toolVersion = "2.0.0-alpha.4"
+//    config.setFrom(file("config/detekt/detekt.yml"))
+//    buildUponDefaultConfig = true
+//}
+
+
+val detektTask = tasks.register<JavaExec>("detekt") {
+    mainClass.set("dev.detekt.cli.Main")
+    classpath = detekt
+
+    val input = projectDir
+    val config = "config/detekt/detekt.yml"
+    val exclude = ".*/build/.*,.*/resources/.*"
+    val params = listOf("-i", input, "-c", config, "-ex", exclude)
+
+    args(params)
+}
+
+// Remove this block if you don't want to run detekt on every build
+tasks.check {
+    dependsOn(detektTask)
 }
