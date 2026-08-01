8e19a7db19ea4e41845417309f9be3023766a3e2
Pablo Quiroga <jtdoe78@gmail.com>
2026-08-01 12:45:11 -0300

    Add JUnit dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,9 +17,11 @@ dependencies {
     testImplementation(kotlin("test"))
     implementation(libs.sqlite.jdbc)
     testImplementation(libs.mockk)
+    testImplementation(libs.junit)
 
     implementation(libs.koin.core)
     testImplementation(libs.koin.test)
+
 }
 
 tasks.test {
